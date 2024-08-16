5d7ce76719e8f15ac6a5aaa950ab9b95cf349c7d
Kingsley Chukwudi Ihemelandu <kijuchihe@gmail.com>
2024-08-16 19:36:07 +0100

    Add new image

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
-import { motion } from "framer-motion";
-import Image from "next/image";
+import { motion } from 'framer-motion';
+import Image from 'next/image';
 
 const Photo = () => {
   return (
@@ -10,7 +10,7 @@ const Photo = () => {
         initial={{ opacity: 0 }}
         animate={{
           opacity: 1,
-          transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+          transition: { delay: 2, duration: 0.4, ease: 'easeIn' },
         }}
       >
         {/* Image */}
@@ -18,12 +18,12 @@ const Photo = () => {
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
-            transition: { delay: 2, duration: 0.4, ease: "easeInOut" },
+            transition: { delay: 2, duration: 0.4, ease: 'easeInOut' },
           }}
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
-            src={"/assets/photo.jpg"}
+            src={'/assets/photo.png'}
             priority
             quality={100}
             fill
@@ -33,28 +33,28 @@ const Photo = () => {
         </motion.div>
         {/* Circle */}
         <motion.svg
-          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
-          fill={"transparent"}
+          className="w-[320px] xl:w-[506px] h-[320px] xl:h-[506px]"
+          fill={'transparent'}
           viewBox="0 0 506 506"
           xmlns="http://www.w3.org/2000/svg"
         >
           <motion.circle
-            cx={"253"}
-            cy={"253"}
-            r={"250"}
+            cx={'253'}
+            cy={'253'}
+            r={'250'}
             stroke="#00ff99"
-            strokeWidth={"4"}
-            strokeLinecap={"round"}
-            strokeLinejoin={"round"}
-            initial={{ strokeDasharray: "24 10 0 0" }}
+            strokeWidth={'4'}
+            strokeLinecap={'round'}
+            strokeLinejoin={'round'}
+            initial={{ strokeDasharray: '24 10 0 0' }}
             animate={{
-              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+              strokeDasharray: ['15 120 25 25', '16 25 92 72', '4 250 22 22'],
               rotate: [120, 360],
             }}
             transition={{
               duration: 20,
               repeat: Infinity,
-              repeatType: "reverse",
+              repeatType: 'reverse',
             }}
           />
         </motion.svg>
